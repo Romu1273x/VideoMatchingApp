fce7ef24dfe075f638a5b888bf429c70cf263d65
Romu <romu1273x@yahoo.co.jp>
2020-12-06 11:25:11 +0900

    Layout for work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     validates :user_id, {presence: true, uniqueness: true}
     validates :hdl_name, {presence: true}
     validates :password, {presence: true}
+
+    def posts
+        # ユーザーの投稿情報を取得
+        return Post.where(user_id: self.user_id)
+    end
 end
